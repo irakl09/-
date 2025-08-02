d58ee831502c534997047d686824c30b0ebd50e8
irakl09 <143933058+irakl09@users.noreply.github.com>
2025-08-02 12:47:08 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,27 @@
-const CACHE_NAME = 'bus-schedule-cache-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/script.js',
-    '/j.png', 
-    '/jj.jpg'
-];
-
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Cache open:', cache);
-                return cache.addAll(urlsToCache);
-            })
-    );
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('volvo-schedule-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/1.png',
+        '/1-512.png',
+        '/manifest.json'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((cachedResponse) => {
-                return cachedResponse || fetch(event.request);
-            })
-    );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (!cacheWhitelist.includes(cacheName)) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
